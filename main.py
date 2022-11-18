981fd84a427955599b770e6fc422cf62acae35b1
BotanicalAmy <amyfolkestad@gmail.com>
2022-11-18 11:35:34 -0700

    Fixed typo in roman_numerals list and a typo in the list comprehension

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ class Solution(object):
         # pull the correct numerals from the list until the input number reaches zero
         while num > 0:
             for rom_val in range(0, len(roman_numeral_values)):
-                if roman_numeral_values[rom_val - 1] <= num < roman_numeral_values[rom_val + 1]:
+                if roman_numeral_values[rom_val] <= num < roman_numeral_values[rom_val+1]:
                     generated_numerals.append(roman_numerals[rom_val])
                     num -= roman_numeral_values[rom_val]
 
@@ -22,7 +22,7 @@ class Solution(object):
 roman_numeral_values = [1, 4, 5, 9, 10, 40, 50, 90, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 2000, 3000, 4000]
 
 # roman numerals at corresponding index values to the roman numeral value list
-roman_numerals = ['I', 'IV', 'V', 'IX', 'X', 'XL', 'L', 'LC', 'C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM',
+roman_numerals = ['I', 'IV', 'V', 'IX', 'X', 'XL', 'L', 'XC', 'C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM',
                   'M', 'MM', 'MMM', 'MMMM']
 
 # ask the user to enter their number
